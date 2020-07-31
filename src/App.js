9f018992c5ec18e8647a5a262f2bea01c4ee024d
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2020-07-31 17:49:38 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
+import theme from "./theme";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+	body{
+		margin:0;
+		padding: 0;
+	}
+
+`;
+
+const Contatiner = styled.div`
+	height: 100vh;
+	width: 100%;
+	background-color: #fafafa;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	overflow: hidden;
+`;
+
+const Card = styled.div`
+	background-color: #fff;
+`;
+
+const Button = styled.button`
+	border-radius: 30px;
+	padding: 25px 15px;
+	&:focus,
+	&:active {
+		outline: none;
+	}
+	background-color: ${(props) => props.theme.successColor};
+`;
+const App = () => {
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Contatiner>
+					<Form />
+				</Contatiner>
+			</ThemeProvider>
+		</>
+	);
+};
+
+const Form = () => (
+	<Card>
+		<Button>Click</Button>
+	</Card>
+);
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+const theme = {
+	mainColor: "#3498db",
+	dangerColor: "#e74c3c",
+	successColor: "#2ecc71",
+};
+export default theme;
